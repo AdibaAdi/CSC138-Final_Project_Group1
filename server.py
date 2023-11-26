b2e21683bab0dd082a693a6756f76ffeec98e900
Adiba Akter <98072437+AdibaAdi@users.noreply.github.com>
2023-11-25 18:15:04 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import sys
 import socket
 import threading
-import sys
 
 # Global dictionary to hold client connections (username: socket)
 clients = {}
@@ -15,10 +14,20 @@ def initialize_server():
 
     port = int(sys.argv[1])
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind the socket to the server address and port
+    
+    # Set the SO_REUSEADDR option on the socket to reuse the socket
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    server_socket.bind(('', port))
     server_socket.listen(10)
     return server_socket
 
+# checks if a string consists of only letters and numbers
+def is_alphanumeric(string: str):
+    for c in string:
+        if not ((c.upper() >= 'A' and c.upper() < 'Z') or (c >= '0' and c <= '9')):
+            return False
+    return True
 
 # Function to handle each client connection
 def client_handler(client_socket, client_address):
@@ -30,14 +39,16 @@ def client_handler(client_socket, client_address):
             if not message:
                 break
 
+            print(f"Connected with {client_address}")
+
             # Split the message into command and arguments
             command, *args = message.split()
 
             # Handle JOIN command
             if command == "JOIN":
                 # Make sure the appropriate number of arguments were provided
-                if len(args) != 1:
-                    client_socket.send("JOIN Error:Bad number of arguments".encode())
+                if len(args) != 1 or not is_alphanumeric(args[0]):
+                    client_socket.send("JOIN Error:Bad Username".encode())
                     break
 
                 username = args[0]
@@ -55,18 +66,16 @@ def client_handler(client_socket, client_address):
 
 
                 # Broadcast join message to other clients
-                broadcast(f"{username} has joined the chat!", username)
+                broadcast(f"{username} Joined the Chatroom!", username)
 
                 # Report back to the client they've successfully been added
                 client_socket.send("JOIN Success:Connected to server!".encode())
-                continue
 
             # Handle LIST command
             elif command == "LIST":
                 # Send a list of all connected clients to the requester
                 client_list = "\n".join(clients.keys())
                 client_socket.send(f"LIST Success:{client_list}".encode())
-                continue
 
             # Handle MESG command
             elif command == "MESG":
@@ -93,7 +102,7 @@ def client_handler(client_socket, client_address):
                 # Broadcast the message to all clients except the sender
                 broadcast(f"{username}: {broadcast_message}", username)
                 client_socket.send(f"BCST Success: Message Broadcast".encode())
-                continue
+            
 
             # Handle QUIT command
             elif command == "QUIT":
@@ -101,8 +110,7 @@ def client_handler(client_socket, client_address):
 
             # Handle unknown commands
             else:
-                client_socket.send("NULL Error: Unknown command".encode())
-                continue
+                client_socket.send("Unknown command".encode())
 
         except Exception as e:
             print(f"Error: {e}")
@@ -110,47 +118,36 @@ def client_handler(client_socket, client_address):
 
     # Remove the client from the list and close the connection
     if username:
-        client_socket.send("QUIT Success: Disconnected from server.".encode())
         client_socket.close()
         del clients[username]
         # Broadcast a message informing others of the client's departure
-        broadcast(f"{username} has left the chat!", username)
+        broadcast(f"{username} Left the Chatroom!", username)
 
 # Function to broadcast a message to all clients
 def broadcast(message, sender=None):
+    message_to_send = message if sender is None else f"{sender}: {message}"
     for username, client_socket in clients.items():
         if username != sender:
             try:
-                client_socket.send(message.encode())
+                client_socket.send(message_to_send.encode())
             except Exception as e:
-                print("Error: {e}")
+                print(f"Broadcast to {username} failed: {e}")
+
 
 # Main function to start the server
 def main():
-    if(len(sys.argv) != 2):
-        print("usage: python3 server.py <svr_port>")
-        sys.exit(1)
-
+    server_socket = initialize_server()
+    print(f"Server started on port {sys.argv[1]}")
     
-    port = sys.argv[1]  # Define the port number
-    server_socket = initialize_server(port)
-    print(f"Server started on port {port}")
-
     # Accept new connections indefinitely
-    try:
-        while True:
-            client_socket, client_address = server_socket.accept()
-            # Start a new thread to handle each client
-            threading.Thread(target=client_handler, args=(client_socket, client_address)).start()
-    except KeyboardInterrupt as ki:
-        server_socket.close()
-        return 0
-    except Exception as e:
-        server_socket.close()
-        print(f"Error: {e}")
-        return 1
-    
+    while True:
+        client_socket, client_address = server_socket.accept()
 
+        # Print the connected client's address - This is the correct location
+        print(f"Connected with {client_address}")
+
+        # Start a new thread to handle each client
+        threading.Thread(target=client_handler, args=(client_socket, client_address)).start()
 # Check if the script is the main program and execute it
 if __name__ == "__main__":
     main()
