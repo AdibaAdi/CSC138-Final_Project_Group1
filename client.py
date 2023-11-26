797bf397405b6d828bf9462a53fbacdc8ffb7d7e
Adiba Akter <98072437+AdibaAdi@users.noreply.github.com>
2023-11-25 18:16:09 -0800

    Update client.py

--- client.py
+++ client.py
@@ -3,75 +3,79 @@
 from socket import *
 import sys
 
-#TCP socket
-cli_socket = socket(AF_INET, SOCK_STREAM)   
-server_name = ''
-server_port = 0
-client_is_connected = False
-
-# send a message to the server then wait to receive the response
-def send_and_receive(message:str):
-    try:
-        cli_socket.sendto(message.encode(),(server_name, server_port))
-        return cli_socket.recv(1024).decode()
-    except:
-        return 'Failed to get Response'
-
-# checks if a string consists of only letters and numbers
+# Function to check if a string is alphanumeric
 def is_alphanumeric(string: str):
-    for c in string:
-        if not ((c.upper() >= 'A' and c.upper() < 'Z') or (c >= '0' and c <= '9')):
-            return False
-    return True
-
-# process response messages from the server
-def process_server_response(message):
-    global client_is_connected
-    print(message)
-    if message.split(':')[0] == "QUIT Success":
-        client_is_connected = False
+    return string.isalnum()
 
 def main():
-    if (len(sys.argv) != 3):
+    # Initialize TCP socket
+    cli_socket = socket(AF_INET, SOCK_STREAM)
+    cli_socket.settimeout(10)
+
+    # Check for correct usage
+    if len(sys.argv) != 3:
         print("Usage: python client.py <server-name> <port>")
         sys.exit(1)
+
     server_name = sys.argv[1]
     server_port = int(sys.argv[2])
 
-    #Try to connect and make sure the connection worked
+    # Try to connect to the server
     try:
         cli_socket.connect((server_name, server_port))
-    except:
-        print("Connection failed!")
+    except Exception as e:
+        print(f"Connection failed: {e}")
         sys.exit(1)
 
-    #TODO: retool this with new paradigm
-    #Prompt user to send JOIN command with their user name
-    join_command_is_valid = False
-    while not join_command_is_valid:
+    # Prompt user to send JOIN command with their username
+    while True:
         join_command = input("Enter JOIN followed by your username: ")
         join_args = join_command.split()
 
-        # if the user's input is invalid in any way, continue to prompt for valid input
-        if len(join_args) != 2: continue
-        if len(join_args[1]) < 1: continue
-        if not is_alphanumeric(join_args[1]): continue
-        if join_args[0] != "JOIN": continue
-        
-        join_command_is_valid = True
-    
-    #Send the JOIN request
-    server_response = send_and_receive(join_command)
-    if server_response.split(':')[0] == "JOIN Success":
-        client_is_connected = True
-    
-    while client_is_connected:
-        userCommand = input(":")
-        process_server_response(send_and_receive(userCommand))
+        # Validate the user's input
+        if len(join_args) == 2 and len(join_args[1]) > 0 and is_alphanumeric(join_args[1]) and join_args[0].upper() == "JOIN":
+            break
+        print("Invalid command format. Please enter JOIN followed by your username.")
+
+    # Send the JOIN request and receive server response
+    server_response = send_and_receive(cli_socket, join_command)
+    print(server_response)
+
+    # Main loop to process user commands
+    while True:
+        user_command = input("Enter command: ")
+        server_response = send_and_receive(cli_socket, user_command)
+        if not process_server_response(cli_socket, server_response):
+            break
 
     cli_socket.close()
-    return 0
 
+# Send a message to the server and wait to receive the response
+def send_and_receive(cli_socket, message: str):
+    try:
+        cli_socket.send(message.encode())  # Use send for TCP socket
+        return cli_socket.recv(1024).decode()
+    except (BrokenPipeError, ConnectionResetError):
+        print("Server has closed the connection.")
+        return None
+    except Exception as e:
+        print(f"Error in communication: {e}")
+        return None
+
+# Process response messages from the server
+def process_server_response(cli_socket, message):
+    if message is None:
+        print("Connection lost.")
+        return False
+
+    print("Server:", message)
+    if message.startswith("JOIN Success"):
+        return True
+    elif "Error" in message or message.startswith("QUIT"):
+        return False
+
+    # Adding additional response processing as needed
+    return True
 
 if __name__ == "__main__":
     main()
